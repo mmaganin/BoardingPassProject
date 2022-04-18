886544f070dbc042c1880e3c0a08a69b3d68073e
SpookySuede <spookysuede@gmail.com>
2022-04-18 11:59:24 -0400

    Initial Views Created
    Created all views with some boilerplate. Committing so Ray may get started.

--- Frontend/boardingpassfrontend/src/App.js
+++ Frontend/boardingpassfrontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {Container } from "react-bootstrap";
+import Welcome from './Views/Welcome';
+import SearchResults from './Views/SearchResults';
+import CustomerInfo from './Views/CustomerInfo';
+import PurchasedTickets from './Views/PurchasedTickets';
+import Ticket from "./Views/Ticket";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <Router>
+            <Container>
+                <Route path="/" component={Welcome} exact />
+                <Route path="/searchresults" component={SearchResults} />
+                <Route path="/customerinfo" component={CustomerInfo} />
+                <Route path="/ticketconfirm" component={Ticket} />
+                <Route path="/purchaselist" component={PurchasedTickets} />
+            </Container>
+        </Router>
+    )
 }
-
-export default App;
