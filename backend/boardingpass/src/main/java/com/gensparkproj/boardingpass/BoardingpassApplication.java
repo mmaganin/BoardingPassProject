b477125cfbc3f2145c125393f90a4a4a0b49837a
Michael Maganini <michaelmags33@gmail.com>
2022-04-18 09:46:03 -0500

    Empty Spring Boot

---
+++ backend/boardingpass/src/main/java/com/gensparkproj/boardingpass/BoardingpassApplication.java
@@ -0,0 +1,13 @@
+package com.gensparkproj.boardingpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardingpassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardingpassApplication.class, args);
+	}
+
+}
---
+++ backend/boardingpass/src/test/java/com/gensparkproj/boardingpass/BoardingpassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gensparkproj.boardingpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardingpassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
