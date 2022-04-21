f88b823dfa965a5672887563e2c45e1950a683f7
Michael Maganini <michaelmags33@gmail.com>
2022-04-21 10:51:33 -0500

    Fetch API from backend working

--- Frontend/boardingpassfrontend/src/Views/SearchResults.js
+++ Frontend/boardingpassfrontend/src/Views/SearchResults.js
@@ -4,23 +4,29 @@ import { useLocation } from "react-router-dom"
 const SearchResults = () => {
   const location = useLocation();
   const [data, setData] = useState(null);
-  const [fetchData, setFetch] = useState(false);
+  const [fetchData, setFetch] = useState(true);
+
+  const json = JSON.stringify({ from_location: location?.state[0], departure_time: location?.state[1] })
+  console.log(json)
 
   useEffect(() => {
     if (fetchData) {
       const payload = {
-        method: 'GET',
-        body: JSON.stringify({from_location: location?.state?.from, departure_time: location?.state?.dateTime})
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: json
       }
-      fetch('http://localhost:8080/searchresults')
+      fetch('http://localhost:8080/searchresults', payload)
         .then(response => response.json())
-        .then(data => setData(data.id));
+        .then(data => setData(data));
     }
   }, [fetchData]);
 
   console.log(data)
   return (
-    <div>SearchResults for {useLocation().state}</div>
+    <div>SearchResults for {location.state}</div>
   )
 }
 
